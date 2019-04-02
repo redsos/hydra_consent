154aa18ba64083c98577fc48bb90a4be1f005392
Jandy <hades.ch@gmail.com>
2019-04-02 23:44:58 +0800

    Update create_client.sh

--- create_client.sh
+++ create_client.sh
@@ -1,8 +1,3 @@
-docker-compose run --rm hydra clients create \
-    --endpoint http://hydra:4444 \
-    --id app \
-    --secret secret \
-    --grant-types authorize_code,refresh_token,client_credentials \
-    --response-types token,code,id_token \
-    --scope openid,offline \
-    --callbacks http://localhost:3000/sessions/callback
+#!/bin/bash
+
+./hydra clients create --endpoint http://tokyo:4445 --id tokyo --secret secret --grant-types authorize_code,refresh_token,client_credentials --response-types token,code,id_token --scope openid,offline --callbacks http://tokyo:3000/sessions/callback --skip-tls-verify

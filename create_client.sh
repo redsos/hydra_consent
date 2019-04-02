aa76c7821b1a9fe3621564a01e643914e85508b0
Jandy <hades.ch@gmail.com>
2019-04-03 00:03:15 +0800

    Update create_client.sh

--- create_client.sh
+++ create_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./hydra clients create --endpoint http://tokyo:4445 --id tokyo --secret secret --grant-types authorize_code,refresh_token,client_credentials --response-types token,code,id_token --scope openid,offline --callbacks http://tokyo:3000/sessions/callback --skip-tls-verify
+./hydra clients create --endpoint http://tokyo.localhost:4445 --id tokyo --secret secret --grant-types authorize_code,refresh_token,client_credentials --response-types token,code,id_token --scope openid,offline --callbacks http://tokyo.localhost:3000/sessions/callback --skip-tls-verify
